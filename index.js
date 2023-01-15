eca85a243a16bc7c227a5a20b723c14697e71dc4
Mamun1646 <mimbd01130004@gmail.com>
2023-01-15 20:48:59 +0600

    version 7.0

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 const execSync = require("child_process").execSync;
-const path = require("path")
+//const path = require("path")
 
 execSync("git clone https://github.com/Mamun1646/nextjs-template.git");
 
-console.log(path.join(__dirname, `/nextjs-template`))
+//console.log(path.join(__dirname, `/nextjs-template`), `/nextjs-template`)
 console.log("npm i starting ")
-execSync("npm i", {cwd:path.join(__dirname, `/nextjs-template`) });
+execSync("npm i", {cwd: `/nextjs-template` });
 console.log("\n\n\n==========Hello, Your done here!===========");
