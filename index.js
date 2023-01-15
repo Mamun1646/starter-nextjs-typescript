7c02bea2b3d1492c4d7600a1a69ded0ba0471bd2
Mamun1646 <mimbd01130004@gmail.com>
2023-01-15 20:01:48 +0600

    hello

--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 const execSync = require("child_process").execSync;
 
 execSync("git clone https://github.com/Mamun1646/nextjs-template.git");
-execSync("cd ./nextjs-template; npm i;");
+execSync("cd nextjs-template; npm i;");
 console.log("\n\n\n==========Hello, Your done here!===========");
